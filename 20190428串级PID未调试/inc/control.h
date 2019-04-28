78d0d4e7ca35de417c6f7c380215538fd44e3783
IADF <38521733+IADF@users.noreply.github.com>
2019-04-28 21:37:10 +0800

    Add files via upload

---
+++ 20190428串级PID未调试/inc/control.h
@@ -0,0 +1,15 @@
+#ifndef _control_H
+#define _control_H
+#include "headfile.h"
+#include  "common.h"
+
+void PIT0_IRQHandler(void);
+int speed_smoothness_output();
+int direction_smoothness_output();
+void motor_control(int motor_right_val,int motor_left_val);
+void Get_encoder(void);
+void angle_output(void);
+void angle_speed_output(void);
+void mpu_normalization_point(void);
+
+#endif
---
+++ 20190428串级PID未调试/inc/middle_line.h
@@ -0,0 +1,7 @@
+#ifndef _middle_h
+#define _middle_h
+
+void find_middle_line();
+
+#endif
+
